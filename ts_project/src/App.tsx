3a69f03483467d6c3a0c96e36ddcf3dd882e4c2b
Divya <divya.raphy@dci-student.org>
2025-05-01 13:48:14 +0200

    check

--- ts_project/src/App.tsx
+++ ts_project/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save 
         </p>
       </div>
       <p className="read-the-docs">
@@ -45,3 +45,4 @@ const Greeting: React.FC<Props> = function({username}):React.JSX.Element{
   )
 }
 export default App
+
